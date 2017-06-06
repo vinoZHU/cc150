30dcd623c57eb9a421a04466c533cdd442e84cea
祝力炀 <liyang@zenzet.com>
2017-06-06 19:01:34 +0800

    Root Cause:判断直线相交
    Verb:
    
    Note:
    
    JIRA-REF:

---
+++ src/main/java/cc150/panduanzhixianxiangjiao/Main.java
@@ -0,0 +1,20 @@
+package cc150.panduanzhixianxiangjiao;
+
+/**
+ * Created by vino on 2017/6/6.
+ * 给定直角坐标系上的两条直线，确定这两条直线会不会相交。
+ 线段以斜率和截距的形式给出，即double s1，double s2，double y1，double y2，分别代表直线1和2的斜率(即s1,s2)和截距(即y1,y2)，请返回一个bool，代表给定的两条直线是否相交。这里两直线重合也认为相交。
+ 测试样例：
+ 3.14,3.14,1,2
+ 返回：false
+ */
+public class Main {
+
+    public boolean checkCrossLine(double s1, double s2, double y1, double y2) {
+        // write code here
+        if (s1 == s2) {
+            return y1 == y2;
+        }
+        return true;
+    }
+}
