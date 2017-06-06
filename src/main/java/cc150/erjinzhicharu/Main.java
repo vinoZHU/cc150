258e6e106066861e1cc1cee34cbab1651a97ba6f
祝力炀 <liyang@zenzet.com>
2017-06-06 09:45:35 +0800

    Root Cause:二进制插入
    Verb:
    
    Note:
    
    JIRA-REF:

---
+++ src/main/java/cc150/erjinzhicharu/Main.java
@@ -0,0 +1,21 @@
+package cc150.erjinzhicharu;
+
+/**
+ * Created by vino on 2017/6/5.
+ * 有两个32位整数n和m，请编写算法将m的二进制数位插入到n的二进制的第j到第i位,其中二进制的位数从低位数到高位且以0开始。
+ 给定两个数int n和int m，同时给定int j和int i，意义如题所述，请返回操作后的数，保证n的第j到第i位均为零，且m的二进制位数小于等于i-j+1。
+ 测试样例：
+ 1024，19，2，6
+ 返回：1100
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Main().binInsert(1024,19,2,6));
+    }
+    public int binInsert(int n, int m, int j, int i) {
+        // write code here
+
+        return n + (m << j);
+    }
+}
