3a56c586f2052e3db662f92f1bb03320d122d9c3
祝力炀 <liyang@zenzet.com>
2017-07-22 23:12:10 +0800

    Root Cause:2的个数
    Verb:
    
    Note:
    
    JIRA-REF:

---
+++ src/main/java/cc150/erdegeshu/Main.java
@@ -0,0 +1,49 @@
+package cc150.erdegeshu;
+
+/**
+ * Created by vino on 2017/7/22.
+ * 请编写一个方法，输出0到n(包括n)中数字2出现了几次。
+ 给定一个正整数n，请返回0到n的数字中2出现了几次。
+ 测试样例：
+ 10
+ 返回：1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().countNumberOf2s(22));
+    }
+
+    public int countNumberOf2s(int n) {
+        // write code here
+//        int count = 0;
+//        int low = 0;
+//        int high = 0;
+//        int cur = 0;
+//        int flag = 1;
+//        while(n/flag!=0)
+//        {
+//            low = n-(n/flag)*flag;
+//            cur = (n/flag)%10;
+//            high = n/(flag*10);
+//            if(cur == 1 || cur == 0)
+//                count += high*flag;
+//            if(cur == 2)
+//                count += high*flag + low +1;
+//            if(cur > 2)
+//                count += (high+1)*flag;
+//            flag *= 10;
+//        }
+//        return count;
+
+//分别计算个位，十位……中2出现的次数，然后求和
+        if (n <= 1) return 0;
+
+        long res = 0, m;
+        for (m = 1;m <= n;m *= 10) {
+            long tmp1 = n / m, tmp2 = n % m;
+
+            res += (tmp1 + 7) / 10 * m + ((tmp1 % 10 == 2) ? (tmp2 + 1) : 0);
+        }
+        return (int)res;
+    }
+}
--- src/main/java/cc150/linleijiafa/Main.java
+++ src/main/java/cc150/linleijiafa/Main.java
@@ -2,6 +2,11 @@ package cc150.linleijiafa;
 
 /**
  * Created by vino on 2017/7/22.
+ * 请编写一个函数，将两个数字相加。不得使用+或其他算数运算符。
+ 给定两个int A和B。请返回A＋B的值
+ 测试样例：
+ 1,2
+ 返回：3
  */
 public class Main {
 
