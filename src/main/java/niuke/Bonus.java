1e17d2f41f92debfda8d74c5a608f7a60fe9f13d
祝力炀 <liyang@zenzet.com>
2017-07-28 16:14:41 +0800

    Root Cause:年终奖
    Verb:
    
    Note:
    
    JIRA-REF:

---
+++ src/main/java/niuke/Bonus.java
@@ -0,0 +1,35 @@
+package niuke;
+
+/**
+ * Created by vino on 2017/7/28.
+ *
+ 小东所在公司要发年终奖，而小东恰好获得了最高福利，他要在公司年会上参与一个抽奖游戏，游戏在一个6*6的棋盘上进行，上面放着36个价值不等的礼物，每个小的棋盘上面放置着一个礼物，
+ 他需要从左上角开始游戏，每次只能向下或者向右移动一步，到达右下角停止，一路上的格子里的礼物小东都能拿到，请设计一个算法使小东拿到价值最高的礼物。
+ 给定一个6*6的矩阵board，其中每个元素为对应格子的礼物价值,左上角为[0,0],请返回能获得的最大价值，保证每个礼物价值大于100小于1000。
+ */
+public class Bonus {
+    public static void main(String[] args) {
+
+    }
+
+    public int getMost(int[][] board) {
+        // write code here
+        int row = board.length;
+        int column = board[0].length;
+        int[][] dp = new int[row][column];
+        dp[0][0] = board[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = board[i][0] + dp[i - 1][0];
+        }
+        for (int j = 1; j < column; j++) {
+            dp[0][j] = board[0][j] + dp[0][j - 1];
+        }
+        for (int k = 1; k < row; k++) {
+            for (int p = 1; p < column; p++) {
+                dp[k][p] = Math.max(dp[k - 1][p], dp[k][p - 1]) + board[k][p];
+            }
+        }
+        return dp[row - 1][column - 1];
+
+    }
+}
