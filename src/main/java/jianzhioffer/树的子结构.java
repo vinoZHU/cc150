2289dadb9de96e1a75be959692e4cca0659bd82e
vino <vino2014zly@gmail.com>
2017-08-11 10:55:13 +0800

    Root Cause:树的子结构
    Verb:
    
    Note:
    
    JIRA-REF:

---
+++ src/main/java/jianzhioffer/树的子结构.java
@@ -0,0 +1,27 @@
+package jianzhioffer;
+
+import java.util.Stack;
+
+/**
+ * Created by vino on 2017/8/11.
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+ */
+public class 树的子结构 {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if (root1 == null || root2 == null)
+            return false;
+
+        return isSubTree(root1, root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+    }
+    private boolean isSubTree(TreeNode node1, TreeNode node2) {
+        if (node2 == null)
+            return true;
+        if (node1 == null)
+            return false;
+        if (node1.val == node2.val) {
+            return isSubTree(node1.left, node2.left) && isSubTree(node1.right, node2.right);
+        }
+        else
+            return false;
+    }
+}
