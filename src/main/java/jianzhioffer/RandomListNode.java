f1b414e34e08e1b823dea73d8304ae0c36dc0719
vino <vino2014zly@gmail.com>
2017-08-11 16:42:20 +0800

    Root Cause:复杂链表的复制
    Verb:
    
    Note:
    
    JIRA-REF:

---
+++ src/main/java/jianzhioffer/RandomListNode.java
@@ -0,0 +1,14 @@
+package jianzhioffer;
+
+/**
+ * Created by vino on 2017/8/11.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
---
+++ src/main/java/jianzhioffer/复杂链表的复制.java
@@ -0,0 +1,42 @@
+package jianzhioffer;
+
+/**
+ * Created by vino on 2017/8/11.
+ * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。（注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+ */
+public class 复杂链表的复制 {
+    public static void main(String[] args) {
+
+    }
+
+    public RandomListNode Clone(RandomListNode pHead)
+    {
+        if (pHead == null)
+            return null;
+        RandomListNode pointer = pHead;
+        while (pointer != null) {
+            RandomListNode tmp = new RandomListNode(pointer.label);
+            tmp.next = pointer.next;
+            pointer.next = tmp;
+            pointer = tmp.next;
+        }
+
+        pointer = pHead;
+        while (pointer != null) {
+            if (pointer.random != null) {
+                pointer.next.random = pointer.random.next;
+            }
+            pointer = pointer.next.next;
+        }
+        pointer = pHead;
+        RandomListNode head = pHead.next;
+        while (pointer.next != null) {
+            RandomListNode tmp = pointer.next;
+            pointer.next = tmp.next;
+            pointer = tmp;
+
+        }
+        return head;
+
+    }
+}
